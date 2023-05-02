db97293ac3ef6a95094e406f8dfced909f6486ba
EmilianoZuno <zussman777@gmail.com>
2023-05-02 14:10:51 -0600

    Tarea9

---
+++ src/main/java/org/example/Tarea9/Main.java
@@ -0,0 +1,9 @@
+package org.example.Tarea9;
+
+import org.example.Tarea9.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundoAWT");
+    }
+}
