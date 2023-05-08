863a9c37386bdf017fd42297a1e0364737421ac5
EmilianoZuno <zussman777@gmail.com>
2023-05-07 23:08:01 -0600

    Tarea 10

---
+++ src/main/java/org/example/Tarea10/Main.java
@@ -0,0 +1,8 @@
+package org.example.Tarea10;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
