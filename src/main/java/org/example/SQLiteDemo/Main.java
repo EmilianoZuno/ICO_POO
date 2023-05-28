63e73c77461d39173922fc59ef32f4362f209f97
EmilianoZuno <126686344+EmilianoZuno@users.noreply.github.com>
2023-05-27 21:29:56 -0600

    Actividad extra 7

--- src/main/java/org/example/SQLiteDemo/Main.java
+++ src/main/java/org/example/SQLiteDemo/Main.java
@@ -1,14 +1,29 @@
 package org.example.SQLiteDemo;
 
+import org.example.SQLiteDemo.modelo.Libro;
 import org.example.SQLiteDemo.persistencia.DemoLibroDB;
+import org.example.SQLiteDemo.persistencia.LibroDAO;
+
+import javax.swing.*;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        DemoLibroDB demo = new DemoLibroDB();
-        demo.insertarStatement();
+        LibroDAO ldao = new LibroDAO();
+        try {
+            Libro res = (Libro) ldao.buscarPorId("1");
+            System.out.println(res);
+            System.out.println("--------------");
+            for (Object lib: ldao.obtenerTodo()) {
+                System.out.println((Libro)lib);
+                
+            }
 
-        System.out.println("Con prepared");
-        demo.insertarPreparedStatement();
+        } catch (SQLException sqle){
+            System.out.println("Error al eliminar");
+            System.out.println(sqle.getMessage());
+        }
+
     }
 }
