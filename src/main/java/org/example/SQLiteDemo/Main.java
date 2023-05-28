d1a20de6f7e6988f2dd0e022f9047faaa8f12df8
EmilianoZuno <126686344+EmilianoZuno@users.noreply.github.com>
2023-05-27 22:46:02 -0600

    Actividad extra 8

--- src/main/java/org/example/SQLiteDemo/Main.java
+++ src/main/java/org/example/SQLiteDemo/Main.java
@@ -1,29 +1,17 @@
 package org.example.SQLiteDemo;
 
+import org.example.SQLiteDemo.controlador.ControladorLibro;
 import org.example.SQLiteDemo.modelo.Libro;
 import org.example.SQLiteDemo.persistencia.DemoLibroDB;
 import org.example.SQLiteDemo.persistencia.LibroDAO;
+import org.example.SQLiteDemo.vista.VentanaLibro;
 
 import javax.swing.*;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        LibroDAO ldao = new LibroDAO();
-        try {
-            Libro res = (Libro) ldao.buscarPorId("1");
-            System.out.println(res);
-            System.out.println("--------------");
-            for (Object lib: ldao.obtenerTodo()) {
-                System.out.println((Libro)lib);
-                
-            }
-
-        } catch (SQLException sqle){
-            System.out.println("Error al eliminar");
-            System.out.println(sqle.getMessage());
-        }
-
+        VentanaLibro view = new VentanaLibro("MVC y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
     }
 }
---
+++ src/main/java/org/example/SQLiteDemo/controlador/ControladorLibro.java
@@ -0,0 +1,28 @@
+package org.example.SQLiteDemo.controlador;
+
+import org.example.SQLiteDemo.modelo.ModeloTablaLibro;
+import org.example.SQLiteDemo.vista.VentanaLibro;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ControladorLibro extends MouseAdapter {
+    private VentanaLibro view;
+    private ModeloTablaLibro modelo;
+
+    public ControladorLibro(VentanaLibro view) {
+        this.view = view;
+        this.view.getBtnCargar().addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getSource() == this.view.getBtnCargar()){
+            modelo = new ModeloTablaLibro();
+            modelo.cargarDatos();
+            this.view.getTblLibro().setModel(modelo);
+            this.view.getTblLibro().updateUI();
+        }
+    }
+}
